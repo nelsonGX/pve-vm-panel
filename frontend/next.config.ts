ff7bca2739bf9d731f93dbadde095c7aeaa994df
nelsonGX <hi@nelsongx.com>
2026-04-03 01:42:08 +0800

    turbopack config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,7 +11,10 @@ const nextConfig: NextConfig = {
         pathname: '/avatars/**',
       },
     ],
-  }
+  },
+  turbopack: {
+    root: path.join(__dirname, '.'),
+  },
 };
 
 export default nextConfig;
