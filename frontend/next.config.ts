a033df3203e8e819bc2cfdb3a9f1d053acd09aa1
nelsonGX <hi@nelsongx.com>
2026-04-03 01:53:27 +0800

    ehh this breaks your computer

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -11,9 +10,6 @@ const nextConfig: NextConfig = {
         pathname: '/avatars/**',
       },
     ],
-  },
-  turbopack: {
-    root: path.join(__dirname, '.'),
   },
 };
 
